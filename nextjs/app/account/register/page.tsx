5ae0a230bd3486ed9fb4195a4ee7bfab51a9bd8d
EmmanuelSWE <112879564+EmmanuelSWE@users.noreply.github.com>
2026-03-27 14:30:53 +0200

    [tests] added script for faster local testing

--- nextjs/app/account/register/page.tsx
+++ nextjs/app/account/register/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { AuthInputField } from "@/app/components/auth/auth-input-field";
 
+type RegisterFormState = {
+  name: string;
+  surname: string;
+  email: string;
+  username: string;
+  password: string;
+};
+
+const INITIAL_STATE: RegisterFormState = {
+  name: "",
+  surname: "",
+  email: "",
+  username: "",
+  password: ""
+};
+
 export default function RegisterPage() {
+  const [form, setForm] = useState<RegisterFormState>(INITIAL_STATE);
+
   return (
     <div className="auth-form-wrap">
       <div className="auth-title-block">
@@ -11,26 +32,50 @@ export default function RegisterPage() {
       </div>
 
       <form className="auth-form">
-        <AuthInputField id="name" type="text" label="Name" placeholder="Ada" value="" icon="<" onChange={() => {}} />
-        <AuthInputField id="surname" type="text" label="Surname" placeholder="Lovelace" value="" icon="<" onChange={() => {}} />
+        <AuthInputField
+          id="name"
+          type="text"
+          label="Name"
+          placeholder="Ada"
+          value={form.name}
+          icon="<"
+          onChange={(name) => setForm((current) => ({ ...current, name }))}
+        />
+        <AuthInputField
+          id="surname"
+          type="text"
+          label="Surname"
+          placeholder="Lovelace"
+          value={form.surname}
+          icon="<"
+          onChange={(surname) => setForm((current) => ({ ...current, surname }))}
+        />
         <AuthInputField
           id="email"
           type="text"
           label="Email address"
           placeholder="ada@example.com"
-          value=""
+          value={form.email}
           icon="@"
-          onChange={() => {}}
+          onChange={(email) => setForm((current) => ({ ...current, email }))}
         />
-        <AuthInputField id="username" type="text" label="User name" placeholder="ada" value="" icon="U" onChange={() => {}} />
+        <AuthInputField
+          id="username"
+          type="text"
+          label="User name"
+          placeholder="ada"
+          value={form.username}
+          icon="U"
+          onChange={(username) => setForm((current) => ({ ...current, username }))}
+        />
         <AuthInputField
           id="password"
           type="password"
           label="Password"
           placeholder="Create password"
-          value=""
+          value={form.password}
           icon="*"
-          onChange={() => {}}
+          onChange={(password) => setForm((current) => ({ ...current, password }))}
         />
 
         <div className="auth-submit-row">
